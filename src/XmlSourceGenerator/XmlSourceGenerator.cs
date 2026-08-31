BinToss/saivineeth100.XmlSourceGenerator

commit 2209e751d191509264573be3c59fccfe6d86e137

fix: prepend source generator header; conditionally enable/restore #nullable,
Fixes [#3](https://github.com/saivineeth100/XmlSourceGenerator/issues/3)

src/XmlSourceGenerator/XmlSourceGenerator.cs
@@ -14,6 +14,20 @@ namespace XmlSourceGenerator
     [Generator]
     public class XmlGenerator : IIncrementalGenerator
     {
+        protected static bool SupportsNullable(Compilation compilation)
+            => compilation is CSharpCompilation csc && csc.LanguageVersion >= LanguageVersion.CSharp8;
+        private const string Header =
+            """
+        ///------------------------------------------------------------------------------
+        /// <auto-generated>
+        ///     This code was generated by XmlSourceGenerator
+        ///
+        ///     Changes to this file may cause incorrect behavior and will be lost if
+        ///     the code is regenerated.
+        /// </auto-generated>
+        ///------------------------------------------------------------------------------
+        """;
+
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
             // Register a syntax provider to find classes with attributes
@@ -79,6 +93,11 @@ namespace XmlSourceGenerator
             
             string namespaceName = classSymbol.ContainingNamespace.ToDisplayString();
             
+            sb.AppendLine(Header);
+            sb.AppendLine();
+            if (SupportsNullable(compilation))
+                sb.AppendLine("#nullable enable");
+            sb.AppendLine();
             sb.AppendLine($"using {Constants.SystemNamespace};");
             sb.AppendLine($"using {Constants.SystemCollectionsGenericNamespace};");
             sb.AppendLine($"using {Constants.SystemXmlLinqNamespace};");
@@ -174,6 +193,8 @@ namespace XmlSourceGenerator
                 }
             }
             sb.AppendLine("}");
+            if (SupportsNullable(compilation))
+                sb.AppendLine($"#nullable restore");
 
             return sb.ToString();
         }

commit 6df696b22ab622669da635ee4683967c6306954c

fix: resolve CS1587 warnings caused by 2209e75

src/XmlSourceGenerator/XmlSourceGenerator.cs
@@ -16,14 +16,14 @@ namespace XmlSourceGenerator
             => compilation is CSharpCompilation csc && csc.LanguageVersion >= LanguageVersion.CSharp8;
         private const string Header =
             """
-        ///------------------------------------------------------------------------------
-        /// <auto-generated>
-        ///     This code was generated by XmlSourceGenerator
-        ///
-        ///     Changes to this file may cause incorrect behavior and will be lost if
-        ///     the code is regenerated.
-        /// </auto-generated>
-        ///------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------
+        // <auto-generated>
+        //     This code was generated by XmlSourceGenerator
+        //
+        //     Changes to this file may cause incorrect behavior and will be lost if
+        //     the code is regenerated.
+        // </auto-generated>
+        //------------------------------------------------------------------------------
         """;
 
         public void Initialize(IncrementalGeneratorInitializationContext context)

tests/XmlSourceGenerator.UnitTests/SourceGeneration/SnapshotTests.cs
@@ -20,14 +20,14 @@ namespace Test
     }
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -97,14 +97,14 @@ namespace Test
     }
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -184,14 +184,14 @@ namespace Test
     }
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -256,14 +256,14 @@ namespace Test
     }
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -345,14 +345,14 @@ namespace Test
     }
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -457,14 +457,14 @@ namespace Test
     }
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -546,14 +546,14 @@ namespace Test
     public partial class LedgerEntry : BaseLedgerEntry {}
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -669,14 +669,14 @@ namespace Test
     public partial class LedgerEntry : BaseLedgerEntry {}
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -777,14 +777,14 @@ namespace Test
     }
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 
@@ -867,14 +867,14 @@ namespace Test
     }
 }";
 
-        var expectedCode = @"///------------------------------------------------------------------------------
-/// <auto-generated>
-///     This code was generated by XmlSourceGenerator
-///
-///     Changes to this file may cause incorrect behavior and will be lost if
-///     the code is regenerated.
-/// </auto-generated>
-///------------------------------------------------------------------------------
+        var expectedCode = @"//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by XmlSourceGenerator
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
 
 #nullable enable
 